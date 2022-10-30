97762e54b0744ee0b1c921f80a1601af29f502e4
warrior-coder <gudkou2003@gmail.com>
2022-10-30 20:58:49 +0300

    Add extra tasks.

--- extra_tasks.sql
+++ extra_tasks.sql
@@ -1,4 +1,73 @@
-/* a) Find PC models having a speed of at least 3.00 */
+-- a) Find PC models having a speed of at least 3.00
+SELECT model
+FROM pc
+WHERE speed > 3.0;
 
-/* b) Find makers producing laptops with hd of at least 100GB. */
+-- b) Find makers producing laptops with hd of at least 100GB.
+SELECT product.maker
+FROM product
+JOIN laptop ON product.model = laptop.model;
+WHERE laptop.hd > 100.0;
 
+-- c) Find the model numbers and price of all products made by maker B.
+SELECT pc.model, pc.price
+FROM pc
+LEFT JOIN product ON pc.model = product.model
+WHERE product.maker = 'B'
+UNION
+SELECT laptop.model, laptop.price
+FROM laptop
+LEFT JOIN product ON laptop.model = product.model
+WHERE product.maker = 'B'
+UNION
+SELECT printer.model, printer.price
+FROM printer
+LEFT JOIN product ON printer.model = product.model
+WHERE product.maker = 'B';
+
+-- c)
+SELECT pc.model, pc.price
+FROM pc
+WHERE pc.model IN (
+    SELECT product.model
+    FROM product
+    WHERE product.maker = 'B'
+)
+UNION 
+SELECT laptop.model, laptop.price
+FROM laptop
+WHERE laptop.model IN (
+    SELECT product.model
+    FROM product
+    WHERE product.maker = 'B'
+)
+UNION 
+SELECT printer.model, printer.price
+FROM printer
+WHERE printer.model IN (
+    SELECT product.model
+    FROM product
+    WHERE product.maker = 'B'
+);
+
+-- d) Find the model numbers of all color laser printers.
+SELECT model
+FROM printer
+WHERE color = 'y' AND type = 'Laser';
+
+-- e) Find those makers producing laptops but not PCs.
+((SELECT DISTINCT maker
+FROM product, laptop
+WHERE product.model = laptop.model)
+
+EXCEPT ALL
+
+(SELECT DISTINCT maker
+FROM product, pc
+WHERE product.model = pc.model));
+
+-- f) Find those hd that occur in two or more PCs.
+SELECT hd
+FROM pc
+GROUP BY hd
+HAVING COUNT(hd) >= 2
--- sql_ex_1.sql
+++ sql_ex_1.sql
@@ -30,20 +30,45 @@ JOIN laptop ON product.model = laptop.model
 WHERE laptop.hd >= 10;
 
 /* 7. Get the models and prices for all commercially available products (of any type) produced by maker B. */
-SELECT pc.model, pc.price 
+SELECT pc.model, pc.price
 FROM pc
-LEFT JOIN product ON product.model = pc.model
+LEFT JOIN product ON pc.model = product.model
 WHERE product.maker = 'B'
 UNION
-SELECT laptop.model, laptop.price 
+SELECT laptop.model, laptop.price
 FROM laptop
-LEFT JOIN product ON product.model = laptop.model
+LEFT JOIN product ON laptop.model = product.model
 WHERE product.maker = 'B'
 UNION
-SELECT printer.model, printer.price 
+SELECT printer.model, printer.price
 FROM printer
-LEFT JOIN product ON product.model = printer.model
+LEFT JOIN product ON printer.model = product.model
 WHERE product.maker = 'B';
+
+/* 7. */
+SELECT pc.model, pc.price
+FROM pc
+WHERE pc.model IN (
+    SELECT product.model
+    FROM product
+    WHERE product.maker = 'B'
+)
+UNION 
+SELECT laptop.model, laptop.price
+FROM laptop
+WHERE laptop.model IN (
+    SELECT product.model
+    FROM product
+    WHERE product.maker = 'B'
+)
+UNION 
+SELECT printer.model, printer.price
+FROM printer
+WHERE printer.model IN (
+    SELECT product.model
+    FROM product
+    WHERE product.maker = 'B'
+);
 
 /* --- 8. Find the makers producing PCs but not laptops. */
 SELECT product.maker
@@ -86,4 +111,8 @@ FROM (
     WHERE product.maker = 'A'
 ) AS t1;
 
-
+/* 15. Get hard drive capacities that are identical for two or more PCs. Result set: hd. */
+SELECT hd
+FROM pc
+GROUP BY hd
+HAVING COUNT(hd) >= 2
